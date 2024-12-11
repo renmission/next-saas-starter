a6fd09507478a52c0926a381b14ba368732b275d
renmission <missionrenjr@gmail.com>
2024-12-12 01:12:17 +0800

    Added API Pages route for magic link issue to fixed

--- lib/email.ts
+++ lib/email.ts
@@ -1,51 +1,31 @@
-import { MagicLinkEmail } from "@/emails/magic-link-email";
 import { EmailConfig } from "next-auth/providers/email";
 import { Resend } from "resend";
 
 import { env } from "@/env.mjs";
-import { siteConfig } from "@/config/site";
-
-import { getUserByEmail } from "./user";
 
 export const resend = new Resend(env.RESEND_API_KEY);
 
 export const sendVerificationRequest: EmailConfig["sendVerificationRequest"] =
-  async ({ identifier, url, provider }) => {
-    const user = await getUserByEmail(identifier);
-    if (!user || !user.name) return;
-
-    const userVerified = user?.emailVerified ? true : false;
-    const authSubject = userVerified
-      ? `Sign-in link for ${siteConfig.name}`
-      : "Activate your account";
+  async (params) => {
+    const { identifier, url, provider } = params;
 
     try {
-      const { data, error } = await resend.emails.send({
-        from: provider.from,
-        to:
-          process.env.NODE_ENV === "development"
-            ? "delivered@resend.dev"
-            : identifier,
-        subject: authSubject,
-        react: MagicLinkEmail({
-          firstName: user?.name as string,
-          actionUrl: url,
-          mailType: userVerified ? "login" : "register",
-          siteName: siteConfig.name,
-        }),
-        // Set this to prevent Gmail from threading emails.
-        // More info: https://resend.com/changelog/custom-email-headers
-        headers: {
-          "X-Entity-Ref-ID": new Date().getTime() + "",
+      const response = await fetch(
+        `${env.NEXT_PUBLIC_APP_URL}/api/send-verification-email`,
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({ identifier, url, provider }),
         },
-      });
+      );
 
-      if (error || !data) {
-        throw new Error(error?.message);
+      if (!response.ok) {
+        throw new Error("Failed to send verification email");
       }
-
-      // console.log(data)
     } catch (error) {
-      throw new Error("Failed to send verification email.");
+      console.error("Error sending verification request:", error);
+      throw new Error("Failed to send verification email");
     }
   };
