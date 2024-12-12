babce1ec5e26e4e7186f6bcb833c6e7d7cd97eab
renmission <missionrenjr@gmail.com>
2024-12-12 10:45:58 +0800

    updated pricing details

--- config/subscriptions.ts
+++ config/subscriptions.ts
@@ -6,15 +6,15 @@ export const pricingData: SubscriptionPlan[] = [
     title: "Starter",
     description: "For Beginners",
     benefits: [
-      "Up to 100 monthly posts",
+      "Free trails features",
       "Basic analytics and reporting",
       "Access to standard templates",
     ],
     limitations: [
-      "No priority access to new features.",
-      "Limited customer support",
-      "No custom branding",
-      "Limited access to business resources.",
+      // "No priority access to new features.",
+      // "Limited customer support",
+      // "No custom branding",
+      // "Limited access to business resources.",
     ],
     prices: {
       monthly: 0,
@@ -29,15 +29,15 @@ export const pricingData: SubscriptionPlan[] = [
     title: "Pro",
     description: "Unlock Advanced Features",
     benefits: [
-      "Up to 500 monthly posts",
+      "Unlimited business",
       "Advanced analytics and reporting",
       "Access to business templates",
       "Priority customer support",
       "Exclusive webinars and training.",
     ],
     limitations: [
-      "No custom branding",
-      "Limited access to business resources.",
+      // "No custom branding",
+      // "Limited access to business resources.",
     ],
     prices: {
       monthly: 19,
@@ -48,26 +48,26 @@ export const pricingData: SubscriptionPlan[] = [
       yearly: env.NEXT_PUBLIC_STRIPE_PRO_YEARLY_PLAN_ID,
     },
   },
-  {
-    title: "Business",
-    description: "For Power Users",
-    benefits: [
-      "Unlimited posts",
-      "Real-time analytics and reporting",
-      "Access to all templates, including custom branding",
-      "24/7 business customer support",
-      "Personalized onboarding and account management.",
-    ],
-    limitations: [],
-    prices: {
-      monthly: 19,
-      yearly: 99,
-    },
-    stripeIds: {
-      monthly: env.NEXT_PUBLIC_STRIPE_BUSINESS_MONTHLY_PLAN_ID,
-      yearly: env.NEXT_PUBLIC_STRIPE_BUSINESS_YEARLY_PLAN_ID,
-    },
-  },
+  // {
+  //   title: "Business",
+  //   description: "For Power Users",
+  //   benefits: [
+  //     "Unlimited posts",
+  //     "Real-time analytics and reporting",
+  //     "Access to all templates, including custom branding",
+  //     "24/7 business customer support",
+  //     "Personalized onboarding and account management.",
+  //   ],
+  //   limitations: [],
+  //   prices: {
+  //     monthly: 19,
+  //     yearly: 99,
+  //   },
+  //   stripeIds: {
+  //     monthly: env.NEXT_PUBLIC_STRIPE_BUSINESS_MONTHLY_PLAN_ID,
+  //     yearly: env.NEXT_PUBLIC_STRIPE_BUSINESS_YEARLY_PLAN_ID,
+  //   },
+  // },
 ];
 
 export const plansColumns = [
